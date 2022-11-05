63992add4f8aa0464c13f549a8895e9935fb194c
LahiruPrasad12 <lahirupr471@gmail.com>
2022-11-05 12:41:42 +0530

    completed

--- DBConfig/index.js
+++ DBConfig/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 dotenv.config({ path: '../config.env' });
 
 //Configured the database connection
-const URL = process.env.DATABASE||'mongodb+srv://LahiruPrasad12:Lahiru12@cluster0.n1wciok.mongodb.net/?retryWrites=true&w=majority';
+const URL = process.env.DATABASE||'mongodb+srv://kavindu:kavindu2973@TicketingSystem.i16lbaf.mongodb.net/?retryWrites=true&w=majority';
 mongoose
     .connect(URL,{
         useNewUrlParser: true,
--- Models/Services.js
+++ Models/Services.js
@@ -2,19 +2,20 @@ const mongoose = require('mongoose');
 
 const servicesSchema = new mongoose.Schema({
   name:{ type: String, required: [true,"Please Enter Service Name"] },
-  origin: { type: String, required: [true,"Please Enter origin Name"] },
-  destination: { type: String, required: [true,"Please Enter destination Name"] },
-  start_time: { type: String, required: [true,"Please Enter start_time"]  },
-  arrival_time: { type: String, required: [true,"Please Enter Item arrival_time"]  },
+  from: { type: String, required: [true,"Please Enter origin Name"] },
+  to: { type: String, required: [true,"Please Enter destination Name"] },
+  time: { type: String, required: [true,"Please Enter start_time"]  },
+  // arrival_time: { type: String, required: [true,"Please Enter Item arrival_time"]  },
   price: { type: Number, required: [true,"Please Enter price"] },
-  bus_num: {type: String, required: [true,"Please Enter bus_num"]},
+  busno: {type: String, required: [true,"Please Enter bus_num"]},
+  routeno: {type: String, required: [true,"Please Enter bus_num"]},
+  roottype: {type: String, required: [true,"Please Enter bus_num"]},
+  noofbus: {type: String, required: [true,"Please Enter bus_num"]},
+  status: {type: String, required: [true,"Please Enter bus_num"]},
+  capacity: {type: String, required: [true,"Please Enter bus_num"]},
   contact_num: {type: Number, required: [true,"Please Enter contact_num"]},
-  type: {
-    type: String,
-    enum: ['AC', 'NON/AC'],
-    default: 'NON/AC'
-  },
+  type: { type: String },
 });
 
-const Service = mongoose.model('Services', servicesSchema);
-module.exports = Service;
+const BusTrip = mongoose.model('bustrip', servicesSchema);
+module.exports = BusTrip;
